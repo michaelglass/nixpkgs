ba7fd59cd60b3555a7c8619950b3e02a1c5fb6a6
Michael Glass <me@mike.is>
2024-07-08 23:25:38 +0200

    mydumper: fix darwin build

--- pkgs/tools/backup/mydumper/default.nix
+++ pkgs/tools/backup/mydumper/default.nix
@@ -34,7 +34,15 @@ stdenv.mkDerivation rec {
     "-DWITH_ZSTD=ON"
   ];
 
-  env.NIX_CFLAGS_COMPILE = "-Wno-error=maybe-uninitialized";
+  env.NIX_CFLAGS_COMPILE = (
+    if stdenv.isDarwin then
+      toString [
+        "-Wno-error=deprecated-non-prototype"
+        "-Wno-error=format"
+      ]
+    else
+      "-Wno-error=maybe-uninitialized"
+  );
 
   postPatch = ''
     # as of mydumper v0.14.5-1, mydumper tries to install its config to /etc
@@ -53,7 +61,7 @@ stdenv.mkDerivation rec {
     homepage = "https://github.com/mydumper/mydumper";
     changelog = "https://github.com/mydumper/mydumper/releases/tag/v${version}";
     license = licenses.gpl3Plus;
-    platforms = platforms.linux;
+    platforms = lib.platforms.unix;
     maintainers = with maintainers; [ izorkin ];
   };
 }
